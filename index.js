1c0f1c784d4552717ad8771a1f9a62daf35296b0
chiroo <ochiroo.c0904@gmail.com>
2020-10-26 13:28:08 +0800

    lastt

---
+++ index.js
@@ -0,0 +1,15 @@
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username=="admin" && password=="1234"){
+        alert("Амжилттай нэвтэрч орлоо");
+        //window.location="1.html";
+        
+        location="1.html";
+    }
+    else{
+        alert("Хэрэглэгчийн нэр эсвэл нууц үг буруу байна");
+    }
+}
+var gg=10;
+document.getElementById("d").innerHTML=gg;
